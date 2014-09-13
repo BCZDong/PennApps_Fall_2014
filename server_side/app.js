57e2681a549237bbc89fd8b057a872c8835f45d3
Bill Dong <chdong@seas.upenn.edu>
2014-09-12 22:53:33 -0400

    app.js

--- server_side/app.js
+++ server_side/app.js
@@ -1,17 +1,12 @@
 var server = require("./server"),
     router = require("./router"),
     requestHandlers = require("./requestHandlers"),
-    pi = require("./pi"),
     ys = require('util'),
     exec = require('child_process').exec;
   
 var handle = {}
-handle["/videoPlayer"] = requestHandlers.videoPlayer;
-handle["/pressureReadings"] = requestHandlers.pressureReadings;
-handle["/ledInputs"] = requestHandlers.ledInputs;
-handle["/beginner"] = requestHandlers.beginner;
-handle["/intermediate"] = requestHandlers.intermediate;
-handle["/advanced"] = requestHandlers.advanced;
+handle["/getClasses"] = requestHandlers.getClasses;
  
 var port = process.env.PORT || 8081;
+console.log("ip address: " + process.env.PORT);
 server.start(router.route, handle, port);
